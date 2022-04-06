291886fa423c9ba25d1a2d343f2660f726b62a84
Selim Fazylov <selim.fazylov1@gmail.com>
2022-04-07 00:34:39 +0300

    save

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+// root.render(<App />);
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
@@ -13,3 +13,9 @@ root.render(<App />);
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+
+const constanta = React.createElement("div", null, React.createElement("h2", null, "Title 1"), React.createElement("p", null, "Some text after title"));
+
+console.log(constanta);
+
+ReactDOM.render(constanta, document.getElementById("root"));
